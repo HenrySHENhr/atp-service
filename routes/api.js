a85062781c94dfb99728f91f4c2c6da6c1efcdc8
HenrySHENhr <shenhanrui@outlook.com>
2019-01-26 22:17:26 +0800

    Complete method: writeExcel

--- routes/api.js
+++ routes/api.js
@@ -10,25 +10,6 @@ let getSolrData = () => {
     port: 8983,
     path: '/solr/shen_sample/select?q=*:*&rows=1&wt=json'
   };
-  // let content = ''
-  // let solrReq = http.request(opt, (res) => {
-  //   console.log(`${opt.method} ${opt.host}:${opt.port}${opt.path}`)
-  //   console.log(`Status: ${res.statusCode}`)
-  //   res.setEncoding('utf8')
-  //   res.on('data', (chunk) => {
-  //     content += chunk
-  //   });
-  //   res.on('end', () => {
-  //     console.log(`Size: ${content.length}`)
-  //     content = JSON.parse(content)
-  //   });
-  // });
-  // solrReq.on('error', (err) => {
-  //   console.error(err)
-  // });
-  // solrReq.end()
-  // return content
-
   return new Promise((resolve, reject) => {
     let req = http.request(opt, function (res) {
       console.log(`${opt.method} ${opt.host}:${opt.port}${opt.path}`)
@@ -50,61 +31,60 @@ let getSolrData = () => {
 }
 
 let writeExcel = (data) => {
-  const address = 'temp/data.xml';
+  const columns = ['id', 'name']
+  const address = 'temp/data.xml'
   fs.exists(address, function (exist) {
     if (!exist) {
       try {
         fs.open(address, 'w+', function (err, fd) {
-          console.log('File creation successful.');
+          console.log('File creation successful.')
         });
       } catch (err) {
-        console.log(`File creation failed. ${err}`);
+        console.log(`File creation failed. ${err}`)
       }
     }
   });
-  var conf = {};
-  conf.stylesXmlFile = address;
-  conf.name = "ATP";
-  conf.cols = [
-    {
-      caption: 'id',
+  let conf = {}
+  conf.stylesXmlFile = address
+  conf.name = "ATP"
+  let cols = []
+  for (let column in columns) {
+    cols.push({
+      caption: columns[column],
       type: 'string'
-    },
-    {
-      caption: 'name',
-      type: 'string'
+    })
+  }
+  conf.cols = cols
+  let rows = []
+  let docRow = []
+  for (let doc in data['response']['docs']) {
+    docRow = []
+    for (let column in columns) {
+      docRow.push(columns[column] in data['response']['docs'][doc] ? data['response']['docs'][doc][columns[column]].toString() : '')
     }
-  ]
-  // conf.rows = [
-  //   ['2016-06-20', '16:22:31'],
-  //   ['2016-06-20', '16:22:31']
-  // ]
-  conf.rows = data['response']['docs']
-  var result = excel.execute(conf)
+    rows.push(docRow)
+  }
+  conf.rows = rows
+  let result = excel.execute(conf)
   return result
 }
 
 router.prefix('/api')
 
 router.get('/', async (ctx, next) => {
+  ctx.type = 'application/json'
+  ctx.body = {
+    title: 'Export Excel Service'
+  }
+})
+
+router.post('/', async (ctx, next) => {
   let solrData = await getSolrData()
   let result = writeExcel(solrData)
   let data = new Buffer(result, 'binary')
   ctx.type = 'application/vnd.openxmlformats'
   ctx.response.attachment('data.xlsx')
   ctx.body = data
-
-  // ctx.type = 'application/json'
-  // ctx.body = {
-  //   title: 'Export Excel Service'
-  // }
-})
-
-router.post('/', async (ctx, next) => {
-  ctx.type = 'application/json'
-  ctx.body = {
-    title: 'Export Excel Service'
-  }
 })
 
 module.exports = router
